efe8c706dc3240281cb495d7c2a241665b8c6869
Heybirader <imrajeev20@gmail.com>
2026-09-06 09:59:34 +0530

    fix: allow ngrok host in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'sulk-confining-ebony.ngrok-free.dev',
+      '.ngrok-free.dev',
+      '.ngrok-free.app',
+      '.ngrok.io'
+    ]
+  }
 })
